6db94fb44e57b5e7b60baec03677235019508677
Vikram Segta <vikramsegta43@gmail.com>
2023-08-07 13:17:38 +0530

    fixed ui consistancy of table items

--- src/pages/table/TableDesc.vue
+++ src/pages/table/TableDesc.vue
@@ -1,58 +1,72 @@
 <template>
   <div class="flex items-start w-full border border-t-0 text-lg py-4 px-4">
     <div class="flex flex-col w-[26%] ml-[3%]">
-      <h1 class="flex font-medium">
-        Name:
-        <span v-if="!edit" class="font-normal ml-2">{{ data?.name }}</span>
-        <input
-          v-else
-          v-model="newData.name"
-          type="text"
-          class="font-normal border border-black px-2 ml-2 outline-none"
-        />
-      </h1>
-      <h1 class="flex items-center font-medium">
-        Topic:
-        <span v-if="!edit" class="font-normal ml-2">{{ data?.topic }}</span>
-        <input
-          v-else
-          v-model="newData.topic"
-          type="text"
-          class="font-normal ml-2 border border-black px-2 mt-1 outline-none"
-        />
-      </h1>
-      <h1 class="flex items-center font-medium">
-        Category:
-        <span v-if="!edit" class="font-normal ml-2">{{ data?.category }}</span>
-        <input
-          v-else
-          v-model="newData.category"
-          type="text"
-          class="font-normal ml-2 border border-black px-2 mt-1 outline-none"
-        />
-      </h1>
+      <div class="flex">
+        <div class="w-20 font-medium mr-2">
+          <h1 class="flex">Name:</h1>
+          <h1 class="flex" :class="edit ? 'mt-1.5' : 'mt-0'">Topic:</h1>
+          <h1 class="flex" :class="edit ? 'mt-1.5' : 'mt-0'">Category:</h1>
+        </div>
+        <div>
+          <div class="flex">
+            <span v-if="!edit" class="font-normal">{{ data?.name }}</span>
+            <input
+              v-else
+              v-model="newData.name"
+              type="text"
+              class="font-normal border border-black px-2 outline-none"
+            />
+          </div>
+          <div class="flex">
+            <span v-if="!edit" class="font-normal">{{ data?.topic }}</span>
+            <input
+              v-else
+              v-model="newData.topic"
+              type="text"
+              class="font-normal border border-black px-2 mt-1 outline-none"
+            />
+          </div>
+          <div class="flex">
+            <span v-if="!edit" class="font-normal">{{ data?.category }}</span>
+            <input
+              v-else
+              v-model="newData.category"
+              type="text"
+              class="font-normal border border-black px-2 mt-1 outline-none"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="flex flex-col w-[40%] ml-5">
-      <h1 class="flex items-center font-medium">
-        Date:
-        <span v-if="!edit" class="font-normal ml-2">{{ data?.date }}</span>
-        <input
-          v-else
-          v-model="newData.date"
-          type="text"
-          class="font-normal ml-2 border border-black px-2 outline-none"
-        />
-      </h1>
-      <h1 class="flex items-center font-medium">
-        Status:
-        <span v-if="!edit" class="font-normal ml-2">{{ data?.status }}</span>
-        <input
-          v-else
-          v-model="newData.status"
-          type="text"
-          class="font-normal ml-2 border border-black px-2 mt-1 outline-none"
-        />
-      </h1>
+    <div class="w-[40%] ml-5">
+      <div class="flex">
+        <div class="w-10 font-medium mr-4">
+          <h1 class="flex">Date:</h1>
+          <h1 class="flex" :class="edit ? 'mt-1.5' : 'mt-0'">Status:</h1>
+        </div>
+        <div>
+          <div class="flex">
+            <span v-if="!edit" class="font-normal ml-2">{{ data?.date }}</span>
+            <input
+              v-else
+              v-model="newData.date"
+              type="text"
+              class="font-normal border border-black px-2 ml-2 outline-none"
+            />
+          </div>
+          <div class="flex">
+            <span v-if="!edit" class="font-normal ml-2">{{
+              data?.status
+            }}</span>
+            <input
+              v-else
+              v-model="newData.status"
+              type="text"
+              class="font-normal border border-black px-2 mt-1 ml-2 outline-none"
+            />
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="flex ml-auto mt-auto">
